b2f9db5b5a6415805f6c27d8e394e4bcd4e4bff8
makayla-star <54339097+makayla-star@users.noreply.github.com>
2019-10-02 11:46:00 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace apphw1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] MovieNames { get; set; }
+        public string [] moviename1 { get; set; }
+        public string [] movienames2 { get; set; }
+        public void OnGet()
+        {
+            MovieNames = new string[] { "Brides Made","Me Before You","It"};
+            moviename1 = new string[] { "Hangover", "After","47 Meters Down" };
+            movienames2 = new string[] { "Little Man", "The Notebook", "BriteBurn" };
+        }
+    }
+}
